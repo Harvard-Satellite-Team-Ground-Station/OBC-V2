c25f480870edacaec670089dc51495239942bc5b
Madison Davis <madisond2302@gmail.com>
2025-06-19 16:55:25 -0700

    Renamed scheduler=>fsm

---
+++ fsm/state_processes/state_charge.py
@@ -0,0 +1,24 @@
+# state_charge.py
+
+
+
+# ++++++++++++++ Imports/Installs ++++++++++++++ #
+import time
+
+
+
+# ++++++++++++++ Functions: Helper ++++++++++++++ #
+def __init__():
+    while True:
+        on_loop()
+
+def on_loop():
+    # TO-DO: do some setup functions
+    # ....
+    time.sleep(5) 
+
+
+
+# ++++++++++++++ Functions: Main ++++++++++++++ #
+print("\n \033[38;5;46m[STATE_CHARGE] [Started]\033[0m \n", flush=True)
+__init__()
