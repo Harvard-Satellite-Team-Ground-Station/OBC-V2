f408292fda3254d0724541551079ea7add1f8510
Madison Davis <madisond2302@gmail.com>
2025-06-19 16:54:38 -0700

    Carried over files from OBC into scheduler

---
+++ scheduler/state_processes/state_detumble.py
@@ -0,0 +1,24 @@
+# state_detumble.py
+
+
+
+# ++++++++++++++ Imports/Installs ++++++++++++++ #
+import time
+
+
+
+# ++++++++++++++ Functions: Helper ++++++++++++++ #
+def __init__():
+    while True:
+        on_loop()
+
+def on_loop():
+    # TO-DO: do some setup functions
+    # ....
+    time.sleep(5) 
+
+
+
+# ++++++++++++++ Functions: Main ++++++++++++++ #
+print("\n \033[38;5;46m[STATE_DETUMBLE] [Started]\033[0m \n", flush=True)
+__init__()
