85f1c1887035bc4a799525e5660870b765e65843
Madison Davis <madisond2302@gmail.com>
2025-06-22 14:45:25 -0700

    Added some minor bootup functionality

--- fsm/state_processes/state_bootup.py
+++ fsm/state_processes/state_bootup.py
@@ -10,14 +10,30 @@ import asyncio
 # ++++++++++++++ Functions: Helper ++++++++++++++ #
 class StateBootup:
     def __init__(self, dp_obj):
+        """
+        Initialize the class object
+        """
         self.dp_obj = dp_obj
+        self._done = False
         self._running = False
     
     async def run(self):
+        """
+        Run the deployment sequence asynchronously
+        """
         self._running = True
-        while self._running:
-            # TODO: do background tasks
-            await asyncio.sleep(2)
+        # TODO: tasks
+        await asyncio.sleep(10)
+        self._done = True
 
     def stop(self):
+        """
+        Manually stop the run()
+        """
         self._running = False
+
+    def is_done(self):
+        """
+        Check if the run() completed on its own
+        """
+        return self._done
